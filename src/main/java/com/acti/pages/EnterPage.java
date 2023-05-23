68dee511288abcd4a5276339a8be787fd13e11a9
sondosissa <sondosissa18@gmail.com>
2023-05-23 14:30:43 -0500

    added author details to enterPage

--- src/main/java/com/acti/pages/EnterPage.java
+++ src/main/java/com/acti/pages/EnterPage.java
@@ -2,6 +2,16 @@ package com.acti.pages;
 
 import com.acti.base.DriverScript;
 
+/*
+ * Name: LoginPage
+ * Description: Manage the locators and methods of EnterPage
+ * Developed By: Sondos
+ * Reviewed By: Srini
+ * Date Reviewed: 05-23-2023
+ * */
+
+
+
 public class EnterPage extends DriverScript{
 
 }
